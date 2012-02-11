1abeefb11498e86d8e3bc9ab23c8aff5918e20bc
estill01 <ehs.stillman@gmail.com>
2012-02-10 21:46:42 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lightroast::Application.config.secret_token = '1c78b3a95b9fd62a6d8c26655eba12553fc2c3fbd107d68241881b19087d51ac8e7ef28c9a322b4d7618864d76fd25b1ede8874d18d386d75c4b3855cd4eae1d'

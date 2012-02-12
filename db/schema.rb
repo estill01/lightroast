47b4b2482c1b2b5857872f495446d9eba5520761
estill01 <ehs.stillman@gmail.com>
2012-02-12 00:27:07 -0600

    models raked

---
+++ db/schema.rb
@@ -0,0 +1,68 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20120212045923) do
+
+  create_table "interests", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "topic"
+    t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "matches", :force => true do |t|
+    t.integer  "meeting_request_id"
+    t.integer  "joiner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "meeting_requests", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "match_count",      :default => 0
+    t.boolean  "closed",           :default => false
+    t.integer  "matches_at_close", :default => 0
+    t.boolean  "canceled",         :default => false
+    t.datetime "expires"
+    t.string   "type",             :default => "solo"
+    t.integer  "matches_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "meetings", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "points", :force => true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "name"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.integer  "phone_number"
+    t.integer  "points_count"
+    t.integer  "meeting_requests_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
